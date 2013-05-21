7c8bf609e194581412ac8e4d458b23a1f1381e4c
Hugo <hugo.ribeiro@comcast.net>
2013-05-21 05:29:30 -0700

    working on R script

--- output/graphScript.py
+++ output/graphScript.py
@@ -1,20 +1,35 @@
 #!/usr/bin/python
 
 #import everything needed
+from optparse import OptionParser
 import xml.etree.ElementTree as ET
 import subprocess
 import sys
 import os
 
-if(len(sys.argv) != 2):
-    print "\nError: not enough arguments!"
+
+#parse args
+parser = OptionParser()
+parser.add_option("-f", "--file", dest="filename",
+                    help = "xml file to read in", type="string", action="store")
+parser.add_option("-g", "--gnuplot", action="store_false", dest="R")
+parser.add_option("-r", "--R", action="store_true", dest="R")
+(option, args) = parser.parse_args()
+
+if(len(option.filename) <= 0):
     sys.exit(0)
 
-tree = ET.parse(sys.argv[1])
+R = False;
+
+if(option.R):
+    R = True;
+
+#set up xml tree
+tree = ET.parse(option.filename)
 root = tree.getroot()
 
+#create directory to dump images and files into
 directory = 'graphs'
-
 if not os.path.exists(directory):
     os.makedirs(directory)
 
@@ -28,7 +43,11 @@ for child in root:
             output.write(temp)
         output.close()
 
-    command = 'gnuplot -e \"filename=\''+directory+'/'+child.get('name')+'\'; outputname=\''+directory+'/'+child.get('name')+'.png\'\" gnuplotScript.cfg'
+    if(R):
+        command = 'r '+directory+'/'+child.get('name')+' '+directory+'/'+child.get('name')+'.png < rScript.R'
+    else:
+        command = 'gnuplot -e \"filename=\''+directory+'/'+child.get('name')+'\'; outputname=\''+directory+'/'+child.get('name')+'.png\'\" gnuplotScript.cfg'
+    
     process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
-    tempOutput = process.communicate()[0]
+    tempOutput = process.communicate()[0]    
     os.remove(directory+'/'+child.get('name'))
