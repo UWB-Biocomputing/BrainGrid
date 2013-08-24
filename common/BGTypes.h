df90f0582df20a2404cbd154c8fed70a1f2aeb76
Chris Burgess <chris.burgess2010@gmail.com>
2013-08-24 00:40:15 -0700

    added missing character in BGTypes.h

--- common/BGTypes.h
+++ common/BGTypes.h
@@ -1,8 +1,6 @@
 #ifndef __BGTYPES_H_
 #define __BGTYPES_H_
 
-#pragma once
-
 // This type is used to measure the difference between 
 // IEEE Standard 754 single and double-precision floating 
 // point values.
@@ -14,35 +12,49 @@
 // slower, but 52 bits are available to store the decimal.
 //
 // We'd like to avoid doubles, if the simulation output doesn't suffer.
-//
 
-// For floats, uncomment the following two lines and comment DOUBLEPRECISION and the other #define
+
+// For floats, uncomment the following two lines and comment DOUBLEPRECISION and
+// the other #define BGFLOAT; vice-versa for doubles.
+
+//#define SINGLEPRECISION
 //#define BGFLOAT float
+#define DOUBLEPRECISION
+#define BGFLOAT double
 
-#ifdef _WIN32
-#define SINGLEPRECISION
-#define BGFLOAT float
-typedef unsigned long long int uint64_t;	//included in inttypes.h, which is not available in WIN32//included in inttypes.h, which is not available in WIN32
-typedef unsigned int       uint32_t; // same deal as above
-#else
-//#define DOUBLEPRECISION
-//#define BGFLOAT double
-#define SINGLEPRECISION
-#define BGFLOAT float
 
-// solution to get rid of typedef redefine errors on different platforms
-#ifdef __APPLE__
-  
-#elif defined __linux__
-	typedef BGFLOAT* PBGFLOAT;
+// TIMEFLOAT is used by the GPU code and needs to be a double
+#define TIMEFLOAT double
+
+
+// Platform Specific (are the typdef's redundant?)
+#ifdef __linux__ 
+typedef unsigned int uint32_t;
+typedef signed int int32_t;
+
+#elif defined __APPLE__
+typedef unsigned int uint32_t;
+typedef signed int int32_t;
+
 #elif defined _WIN32 || defined _WIN64
-	typedef __int32 int32_t;
-	typedef unsigned __int32 uint32_t;
+typedef unsigned __int32 uint32_t;		// included in inttypes.h, which is not 
+										// available in WIN32
+typedef signed __int32 int32_t;
+typedef unsigned long long int uint64_t;
+
 #else
 #error "unknown platform"
-#endif
-#endif // _WIN32
+#endif // Platform Specific
 
-typedef BGFLOAT *PBGFLOAT;
+
+// AMP
+#ifdef USE_AMP
+#define GPU_COMPAT_BOOL uint32_t
+#else
+#define GPU_COMPAT_BOOL bool
+#endif // AMP
+
 
 #endif // __BGTYPES_H_
+
+
