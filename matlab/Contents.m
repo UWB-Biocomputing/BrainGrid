4f4aeda92d136775bb1eac02d652460d14d475a5
stiber <stiber@u.washington.edu>
2016-03-04 12:06:45 -0800

    First cleanup of existing files
    * Flattened directory structure
    * Added boilerplate Contents.m
    * Removed object code
    * Commented readMatrix.cpp
    * Deleted obsolete mex function code

---
+++ matlab/Contents.m
@@ -0,0 +1,10 @@
+% SCRIPTS
+%
+% Files
+%   batch         - batch.m
+%   batch_h5      - batch.m
+%   burstiness2   - BURSTINESS    Plot burstiness index vs. time for spike records
+%   hist_nconnect - plots histgram of number of synapses (connections)
+%   plot_channels - 
+%   plotradii     - Plot circles showing connectivity radii
+%   plotrates     - Plot circles showing firing rate
