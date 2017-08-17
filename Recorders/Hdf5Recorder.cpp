42b578a254d03bec4ca4ad929ee75eeda0db5b8e
Fumitaka Kawasaki <fumik@u.washington.edu>
2017-08-17 16:15:30 -0700

    Fixed #203
    Hdf5Recorder::saveSimData() doesn't write xloc or yloc correctly.
    Convert VectorMatrix to int array and write it at
    Hdf5Recorder::saveSimData().

--- Recorders/Hdf5Recorder.cpp
+++ Recorders/Hdf5Recorder.cpp
@@ -333,12 +333,20 @@ void Hdf5Recorder::saveSimData(vector<Cluster *> &vtClr, vector<ClusterInfo *> &
         }
 
         // Write the neuron location matrices
-        dataSetXloc->write(m_model->getLayout()->xloc, PredType::NATIVE_INT);
-        dataSetYloc->write(m_model->getLayout()->yloc, PredType::NATIVE_INT);
+        int* iXloc = new int[m_sim_info->totalNeurons];
+        int* iYloc = new int[m_sim_info->totalNeurons];
+        for (int i = 0; i < m_sim_info->totalNeurons; i++) {
+            // convert VectorMatrix to int array
+            iXloc[i] = (*m_model->getLayout()->xloc)[i];
+            iYloc[i] = (*m_model->getLayout()->yloc)[i];
+        }
+        dataSetXloc->write(iXloc, PredType::NATIVE_INT);
+        dataSetYloc->write(iYloc, PredType::NATIVE_INT);
+        delete[] iXloc;
+        delete[] iYloc;
 
         int* iNeuronTypes = new int[m_sim_info->totalNeurons];
-        for (int i = 0; i < m_sim_info->totalNeurons; i++)
-        {
+        for (int i = 0; i < m_sim_info->totalNeurons; i++) {
             iNeuronTypes[i] = neuronTypes[i];
         }
         dataSetNeuronTypes->write(iNeuronTypes, PredType::NATIVE_INT);
