1ecb48a6ff3edf533c4e1eaff6183506610ae777
etcadinfinitum <lizzy.presland@gmail.com>
2019-07-19 18:45:06 -0700

    add tests for string & int value retrieval
    At this point, string value retrieval is well-tested in my opinion.
    Integer retrieval still needs tests validating that the integer was
    not set correctly for non-integer XML elements.
    
    Also added the method declarations for testing value retrievals of
    all other types.

--- Tests/run.sh
+++ Tests/run.sh
@@ -10,5 +10,11 @@ if [[ $? != 0 ]]; then
     exit 0
 fi
 
+echo -e "\nTESTING EXECUTION......"
 ./test
+
+echo -e "\nTESTING WITH VALGRIND......"
+valgrind ./test &> valgrind_out.txt
+cat valgrind_out.txt | grep -P -e '( running|in use at exit|total heap usage|no leaks are possible)'
+rm valgrind_out.txt
 rm test
--- Tests/tests_ParameterManager.cpp
+++ Tests/tests_ParameterManager.cpp
@@ -20,36 +20,87 @@ bool testConstructor() {
 }
 
 bool testValidXmlFileReading() {
+    cout << "\nEntered test method for reading valid + invalid XML files" << endl;
     ParameterManager* pm = new ParameterManager();
     // does the object get destroyed?
     string valid[] = {"../configfiles/test-medium-100.xml", "../configfiles/test-large-conected.xml", "../configfiles/test-medium.xml", "../configfiles/test-tiny.xml"};
     string invalid[] = {"../Core/BGDriver.cpp", "./this/path/doesnt/exist", "../configfiles/test-large-connected.xml", ""};
     for (int i = 0; i < 4; i++) 
         assert(pm->loadParameterFile(valid[i]));
+    cout << "Beginning tests for invalid paths (failure expected)" << endl;
     for (int i = 0; i < 4; i++) 
         assert(!pm->loadParameterFile(invalid[i]));
     delete pm;
-    return true;
+    return true;        // tests haven't crashed
 }
 
 bool testValidStringTargeting() {
     /*
      * Testing string retrieval for ../configfiles/test-medium-500.xml
      */
+    cout << "\nEntered test method for targeting strings in XML file" << endl;
     ParameterManager* pm = new ParameterManager();
     if (pm->loadParameterFile("../configfiles/test-medium-500.xml")) {
-        string xpaths[] = {"/BGSimParams/SimInfoParams/"};
-        string result[] = {};
+        string valid_xpaths[] = {"/BGSimParams/SimInfoParams/OutputParams/stateOutputFileName/text()", "//stateOutputFileName/text()", "//NeuronsParams/@class"};
+        string result[] = {"results/test-medium-500-out.xml", "results/test-medium-500-out.xml", "AllLIFNeurons"};
         string s;
+        for (int i = 0; i < 3; i++) {
+            cout << "Testing xpath: " << valid_xpaths[i] << endl;
+            assert(pm->getStringByXpath(valid_xpaths[i], s));
+            cout << "\tretrieved: " << s << endl;
+            assert(s == result[i]);
+            cout << "\tsucceeded" << endl;
+        }
+    } else {
+        // expecting test filename to be valid
+        return false;
     }
+    delete pm;
+    return true;        // tests haven't crashed
+}
+
+bool testValidIntTargeting() {
+    cout << "\nEntered test method for targeting integers in XML file" << endl;
+    ParameterManager* pm = new ParameterManager();
+    cout << "Testing valid integers..." << endl;
+    if (pm->loadParameterFile("../configfiles/test-medium-500.xml")) {
+        string valid_xpath[] = {"//maxFiringRate/text()", "//PoolSize/x/text()", "//PoolSize/y/text()", "//PoolSize/z/text()", "//Seed/value/text()", "//numSims/text()"};
+        int result[] = {200, 30, 30, 1, 1, 500};
+        int val;
+        for (int i = 0; i < 6; i++) {
+            cout << "Testing xpath: " << valid_xpath[i] << endl;
+            assert(pm->getIntByXpath(valid_xpath[i], val));
+            cout << "\tretrieved: " << val << endl;
+            assert(val == result[i]);
+            cout << "\tsucceeded" << endl;
+        }
+    }
+    delete pm;
+    return true;
+}
+
+bool testValidFloatTargeting() {
+    return true;
+}
+
+bool testValidDoubleTargeting() {
+    return true;
 }
 
 int main() {
+    cout << "\nRunning tests for ParameterManager.cpp functionality..." << endl;
     bool success = testConstructor();
     if (!success) return 1;
     success = testValidXmlFileReading();
     if (!success) return 1;
     success = testValidStringTargeting();
+    if (!success) return 1;
+    cout << "About to run testValidIntTargeting()..." << endl;
+    success = testValidIntTargeting();
+    if (!success) return 1;
+    success = testValidFloatTargeting();
+    if (!success) return 1;
+    success = testValidDoubleTargeting();
     if (!success) return 1;
     return 0;
 }
