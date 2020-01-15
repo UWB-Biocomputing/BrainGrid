9715646e45771e952e20d95ee96fcf0b0e4e9d02
Emily Hsu <eeemily17@gmail.com>
2020-01-15 14:44:08 -0800

    update

--- Core/BGDriver.cpp
+++ Core/BGDriver.cpp
@@ -134,7 +134,7 @@ int main(int argc, char* argv[]) {
 #if defined(USE_GPU)
         //print out weights on the GPU
         for(int i = 0; i < vtClr.size(); i++) {
-            dynamic_cast<GPUSpikingCluster *> (vtClr[i])->printGPUPropsCluster();
+            dynamic_cast<GPUSpikingCluster *> (vtClr[i])->printGPUPropsCluster(vtClrInfo[i]);
         }
 #endif // USE_GPU
 
