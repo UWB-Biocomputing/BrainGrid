a24926f9891c71fa38258f1a712205e0de840f5b
Emily Hsu <eeemily17@gmail.com>
2020-01-18 15:19:55 -0800

    minor_updates

--- Matrix/Matrix.h
+++ Matrix/Matrix.h
@@ -68,7 +68,6 @@ public:
   */
   virtual void Print(ostream& os) const = 0;
 
-#if !defined(USE_GPU)
   //! Cereal
   /*template<class Archive>
   static void load_and_construct(Archive& ar, cereal::construct<Matrix>& construct);
@@ -79,7 +78,6 @@ public:
   template<class Archive>
   void load(Archive & archive);*/
 
-#endif 
 
 protected:
 
@@ -144,7 +142,6 @@ ostream& operator<<(ostream& os, const Matrix& obj);
 
 //! Cereal Serialization/Deserialization Method
 
-#if !defined(USE_GPU)
 
 /*template<class Archive>
 void Matrix::save(Archive & archive) const {
@@ -170,6 +167,5 @@ void Matrix::load_and_construct( Archive & ar, cereal::construct<Matrix> & const
   construct( type2, init2, rows2, columns2, multiplier2 );
 }*/
 
-#endif // !USE_GPU
 
 #endif
