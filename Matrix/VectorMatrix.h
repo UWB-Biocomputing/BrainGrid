1215fcb694d320d6e5c485347cc51a5060b0fab2
Emily Hsu <eeemily17@gmail.com>
2020-01-18 16:58:23 -0800

    minor_updates

--- Matrix/VectorMatrix.h
+++ Matrix/VectorMatrix.h
@@ -63,7 +63,7 @@ const VectorMatrix exp(const VectorMatrix& v);
 //#include <cereal/types/polymorphic.hpp> //for inheritance
 //#include <cereal/types/base_class.hpp> //inherit data member from base class
 //#include <cereal/access.hpp> //for load and construct
-#include <cereal/types/vector.hpp>
+//#include <cereal/types/vector.hpp>
 //#include <vector>
 //#include <cereal/types/string.hpp> // for string
 
