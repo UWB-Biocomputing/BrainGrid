438d1fa1b8a7efb39b161deb86b98fd0088041cf
Emily Hsu <eeemily17@gmail.com>
2020-03-15 00:17:18 -0700

    edit_print_gpusynapseprops

--- Core/BGDriver.cpp
+++ Core/BGDriver.cpp
@@ -367,7 +367,7 @@ void printKeyStateInfo(SimulationInfo *simInfo)
 {        
 #if defined(USE_GPU)
     // Prints out SynapsesProps on the GPU
-    dynamic_cast<AllSpikingSynapses *>(dynamic_cast<Model *>(simInfo->model)->m_synapses)->printGPUSynapsesProps(); 
+    dynamic_cast<GPUSpikingModel *>(simInfo->model)->printGPUSynapsesProps(); 
     
     // Prints out radii on the GPU (only if it is a connGrowth model)
     if(dynamic_cast<ConnGrowth *>(dynamic_cast<Model *>(simInfo->model)->m_conns) != nullptr) {
